e4fe84717d35932ed75dafd401836e1c3a4bdb8c
Jonah <Jonahkmoses@gmail.com>
2013-12-08 15:08:12 -0700

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
 
   def new
-    @user = User.new
+    @user  = User.new
     @order = current_order
   end
 
