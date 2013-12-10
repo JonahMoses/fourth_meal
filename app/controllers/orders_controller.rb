f56ea35a130765f74e9a62881cf148c3c9550d08
Antony Siegert <agsiegert@hotmail.com>
2013-12-09 17:17:36 -0700

    added email conformation for guest completed order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -67,6 +67,7 @@ class OrdersController < ApplicationController
       current_user.save
       current_order.update_attributes(status: "paid")
       order = current_order
+      UserMailer.order_confirmation(current_user, current_order).deliver
       session[:order_id] = nil
       redirect_to confirmation_order_path(order)
     else
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe UserMailer do
-  describe "order_confirmation" do
+  describe "logged in user's order_confirmation" do
     user = User.create(email: "me@example.com",
                         full_name: "Antony",
                         password: "hellothere",
@@ -17,6 +17,30 @@ describe UserMailer do
 
     it "renders the body" do
       mail.body.encoded.should match("Thank you for ordering with FoodFight!")
+    end
+  end
+
+  describe "guest user's order confirmation" do
+    user = User.create(email: "me@example.com",
+                        full_name: "Antony",
+                        credit_card_number: "1234123412341234",
+                        billing_street: "123 Main st",
+                        billing_city: "Denver",
+                        billing_state: "CO",
+                        billing_zip_code: "12345",
+                        guest: true)
+    order = Order.create(user_id: user.id, restaurant_id: 100)
+    let(:mail) { UserMailer.order_confirmation(user, order)}
+
+    it "renders the headers" do
+      mail.subject.should eq("Thank you for your order!")
+      mail.to.should eq(["me@example.com"])
+      mail.from.should eq(["foodfightinfo@gmail.com"])
+    end
+
+    it "renders the body" do
+      mail.body.encoded.should match("Thank you for ordering with FoodFight!")
+      mail.body.encoded.should match("User Full Name: Antony")
     end
   end
 
