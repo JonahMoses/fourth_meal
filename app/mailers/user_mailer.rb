d900e22abceb52f488839d971e4da8977bbd0fc1
Antony Siegert <agsiegert@hotmail.com>
2013-12-09 18:19:57 -0700

    repaired email notifications for signup, user and guest order conf

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,15 +4,15 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = 'http://fourth_meal.herokuapp.com/log_in'
-    mail({to: @user.email,
-          subject: 'Welcome to Food Fight'})
+    mail(to: @user.email,
+          subject: 'Welcome to Food Fight')
   end
 
   def order_confirmation(current_user, order)
     @user = current_user
     @order = order
-    mail({to: @user.email,
-         subject: 'Thank you for your order!'})
+    mail(to: @user.email,
+         subject: 'Thank you for your order!')
   end
 
 end
